a741b9677592eb571e9d06e26755e4c20b4aec2f
Baimanchick <baika132213@gmail.com>
2023-11-20 17:36:09 +0600

    navbar, homePage 1/3

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,49 @@
 import React from "react";
+import Gul from "../css/images/gulshar.jpg";
+import "../css/home.scss";
+import { useNavigate } from "react-router-dom";
 
 function HomePage() {
-  return <div>HomePage</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="main-content">
+      {/*  */}
+      <div className="bio-main">
+        <div className="bio-container">
+          {/*  */}
+          <div className="bio-wrapper-left">
+            <img src={Gul} />
+          </div>
+
+          <div className="bio-wrapper-right">
+            <div className="bio-title">
+              <h2 className="bio-h2">Гульшаркан Оконовна</h2>
+              <h3 className="bio-h3">Бизнесвумен. Депутат ЖК. Красивая.</h3>
+              <p className="bio-p-1">
+                У каждого человека есть свое, уникальное предназначение. Мы
+                должны стремиться оставить после себя что-то поистине хорошее. В
+                этом есть суть формирования культурного общества.
+              </p>
+              <p className="bio-p-2">
+                Моя задача минимум, развивать любимый спорт на максимум.
+                Помогать в реализации футбольных событий Всероссийского масштаба
+                и стремиться к улучшению мира вокруг себя.
+              </p>
+              <button onClick={() => navigate("/about")} className="bio-button">
+                Обо мне ⇨
+              </button>
+            </div>
+          </div>
+
+          {/*  */}
+
+          {/*  */}
+        </div>
+      </div>
+      {/*  */}
+    </div>
+  );
 }
 
 export default HomePage;
