7e0cfec76b6978d719074a50d917961716c380f0
Baimanchick <baika132213@gmail.com>
2023-11-29 07:09:54 +0600

    about me page, contact page and fixed bags

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import "../css/contact.scss";
+import { useNavigate } from "react-router-dom";
 
 function ContactPage() {
-  return <div>ContactPage</div>;
+  const navigate = useNavigate();
+  return (
+    <div className="main-contetnt">
+      <header className="about-header">
+        <div className="about-title">
+          <span>
+            <strong onClick={() => navigate("/")}>НА ГЛАВНУЮ</strong> ► КОНТАКТЫ
+          </span>
+        </div>
+        <h2 className="about-h2">Контакты</h2>
+      </header>
+      <section className="contact-section">
+        <h2>Почта</h2>
+        <h3>kargo2016@gmail.com</h3>
+      </section>
+    </div>
+  );
 }
 
 export default ContactPage;
